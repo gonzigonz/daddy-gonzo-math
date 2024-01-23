aa39b810dab51e136b9539b98dae6e8a65ef71f6
Gonzalo Lucero <gonzigonz@users.noreply.github.com>
2024-01-22 23:36:59 -0800

    NextJS Github action added

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+    basePath: isProd ? '/daddy-gonzo-math' : '',
+    output: 'export',
+   
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+   
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+   
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+  }
+   
+  module.exports = nextConfig
