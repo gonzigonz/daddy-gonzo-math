3b840d274e99176cb7f001537b8828eed5d9d0b2
Gonzalo Lucero <gonzigonz@users.noreply.github.com>
2024-01-24 10:21:46 +0000

    Progress grid added

--- app/page.tsx
+++ app/page.tsx
@@ -6,49 +6,91 @@ import { useState } from "react"
 // const vercel_svg_path = `${config.basePath}/vercel.svg`
 // const next_svg_path = `${config.basePath}/next.svg`
 
+class Card {
+  status: string
+  expression: string
+  constructor(expression: string) {
+    this.status = "";
+    this.expression = expression;
+  }
+  className() {
+    switch (this.status) {
+      case "pass":
+        return "text-green-600 bg-green-100";
+      case "fail":
+        return "text-red-600 bg-red-100";
+      default:
+        return "text-grey-600 bg-grey-100";
+    }
+  }
+  icon() {
+    switch (this.status) {
+      case "pass":
+        return "\u2713";
+      case "fail":
+        return "\u2717";
+      default:
+        return "-";
+    }
+  }
+}
+
 let countOfFactors = 0;
-const cards: string[] = [];
-const factors = [1, 2, 3, 4, 5, 6, 10];
+const cards: Card[] = [];
+const factors = [0, 2, 3, 4, 5, 6, 8];
 for (let f of factors) {
-  for (let i = 0; i < 13; i++) {
-    cards[countOfFactors] = `${f}*${i}`;
-    // console.log(cards[countOfFactors]);
+  for (let i = 1; i < 13; i++) {
+    cards[countOfFactors] = new Card(`${f}*${i}`);
     countOfFactors++;
   }
 }
 countOfFactors;
 
 export default function Home() {
-  const [answer, setAnswer] = useState('');
+  const [userInput, setUserInput] = useState('');
   const [counter, setCounter] = useState(0)
-  const [expression, setExpression] = useState(cards[0]);
-  // console.log(`counter: '${counter}', expression: '${expression}', answer: '${answer}'`);
+  const [card, setCard] = useState(cards[0]);
+  // console.log(`counter: '${counter}', expression: '${card.expression}', input: '${userInput}'`);
 
   const handleButtonClick = async (value: string) => {
-    if (value === 'GO') {
-      try {
-        const result = eval(expression).toString()
-        if (answer === result) {
-          setAnswer("correct");
-        } else {
-          setAnswer("wrong");
-        }
+    if (value === 'AC') {
+      setUserInput('Resetting...');
+      await new Promise(f => setTimeout(f, 1000));
 
-        await new Promise(f => setTimeout(f, 1000));
-
-        setAnswer('');
-        let nextIndex = counter === countOfFactors ? 0 : counter + 1;
-        setCounter(nextIndex)
-        setExpression(cards[nextIndex]);
-      } catch (error) {
-        setAnswer("Error");
+      for (var c of cards) {
+        c.status = "";
       }
-    } else if (value === 'C') {
-      setAnswer('');
+      setUserInput('');
       setCounter(0);
-      setExpression(cards[0]);
+      setCard(cards[0]);
     } else {
-      setAnswer((prevAnswer) => prevAnswer + value )
+      if (userInput === "correct" || userInput === "wrong") {
+        return;
+      }
+
+      const newValue = userInput + value;
+      setUserInput(newValue);
+
+      const correctAnswer = eval(card.expression).toString();
+      if (newValue.length === correctAnswer.length) {
+        try {
+          if (newValue === correctAnswer) {
+            card.status = "pass";
+            setUserInput("correct");
+          } else {
+            card.status = "fail";
+            setUserInput("wrong");
+          }
+          await new Promise(f => setTimeout(f, 500));
+
+          setUserInput('');
+          let nextIndex = counter === countOfFactors - 1 ? 0 : counter + 1;
+          setCounter(nextIndex)
+          setCard(cards[nextIndex]);
+        } catch (error) {
+          setUserInput("Error");
+        }
+      }
     }
   }
 
@@ -56,23 +98,31 @@ export default function Home() {
     '7', '8', '9',
     '4', '5', '6',
     '1', '2', '3',
-    'C', '0', 'GO'
+    'AC', '0'
   ]
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-4xl mb-10 font-bold">DADDY GONZO MATH</h1>
       <div className="bg-white p-6 rounded-lg shadow-lg">
+        <div className="grid grid-cols-12">
+          {cards.map((card, index) => (
+            <button
+              key={index}
+              className={card.className()}
+            >{card.icon()}</button>
+          ))}
+        </div>
         <input
           type="text"
           className="w-full mb-2 text-3xl border-b-2 border-gray-400 focus:outline-none"
-          value={expression}
+          value={card.expression}
           readOnly
         />
         <input
           type="text"
           className="w-full mb-4 text-4xl font-bold focus-outline"
-          value={answer}
+          value={userInput}
           readOnly
         />
         <div className="grid grid-cols-3 gap-2">
@@ -81,9 +131,7 @@ export default function Home() {
               key={btn}
               onClick={() => handleButtonClick(btn)}
               className="text-4xl bg-gray-300 hover:bg-gray-400 p-2 rounded-lg"
-            >
-              {btn}
-            </button>
+            >{btn}</button>
           ))}
         </div>
       </div>
