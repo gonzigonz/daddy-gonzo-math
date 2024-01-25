d940fb58302ea20041852ad78367d46fd2d98095
Gonzalo Lucero <gonzigonz@users.noreply.github.com>
2024-01-25 07:49:16 +0000

    Addition operation added

--- app/page.tsx
+++ app/page.tsx
@@ -6,29 +6,64 @@ import { useState } from "react"
 // const vercel_svg_path = `${config.basePath}/vercel.svg`
 // const next_svg_path = `${config.basePath}/next.svg`
 
+const TICK_MARK = "\u2713";
+const CROSS_MARK = "\u2717";
+const DEL_SYMBOL = "\u232b";
+const ROTATE_SYMBOL = "\u21bb";
+
 class Card {
-  status: string
-  expression: string
-  constructor(expression: string) {
-    this.status = "";
-    this.expression = expression;
+  readonly term1: number
+  readonly term2: number
+  operator: string = ""
+  status: string = ""
+
+  constructor(first_term: number, second_term: number, op: string) {
+    this.term1 = first_term;
+    this.term2 = second_term;
+    this.reinitialize(op);
   }
-  className() {
+
+  reinitialize(op: string) {
+    this.operator = op;
+    this.status = "";
+  }
+
+  expression(): string {
+    return `${this.term1} ${this.operator === "+" ? "+" : "x"} ${this.term2} =`;
+  }
+
+  correctAnswer(): string {
+    let ans: number;
+    switch (this.operator) {
+      case "*":
+        ans = this.term1 * this.term2;
+        break;
+      case "+":
+        ans = this.term1 + this.term2;
+        break;
+      default:
+        ans = NaN;
+    }
+    return ans.toString();
+  }
+
+  className(): string {
     switch (this.status) {
       case "pass":
-        return "text-green-500 bg-green-100";
+        return "text-green-500 bg-green-500/25";
       case "fail":
-        return "text-red-500 bg-red-100";
+        return "text-red-500 bg-red-500/25";
       default:
-        return "text-sky-500";
+        return "text-sky-500/50";
     }
   }
-  icon() {
+
+  icon(): string {
     switch (this.status) {
       case "pass":
-        return "\u2713";
+        return TICK_MARK;
       case "fail":
-        return "\u2717";
+        return CROSS_MARK;
       default:
         return "-";
     }
@@ -37,36 +72,33 @@ class Card {
 
 let countOfFactors = 0;
 const cards: Card[] = [];
-const factors = [0, 2, 3, 4, 5, 6, 8];
+const factors = [2, 3, 4, 5, 6, 8];
 for (let f of factors) {
   for (let i = 1; i < 13; i++) {
-    cards[countOfFactors] = new Card(`${f}*${i}`);
+    cards[countOfFactors] = new Card(f, i, "+");
     countOfFactors++;
   }
 }
 countOfFactors;
 
 export default function Home() {
+  const [operation, setOperation] = useState('+');
   const [userInput, setUserInput] = useState('');
   const [counter, setCounter] = useState(0)
   const [card, setCard] = useState(cards[0]);
-  // console.log(`counter: '${counter}', expression: '${card.expression}', input: '${userInput}'`);
 
   const handleButtonClick = async (value: string) => {
-    if (value === 'AC') {
-      setUserInput('clearing...');
-      await new Promise(f => setTimeout(f, 500));
-
+    if (value === ROTATE_SYMBOL) {
       for (var c of cards) {
-        c.status = "";
+        c.reinitialize(c.operator)
       }
       setUserInput('');
       setCounter(0);
       setCard(cards[0]);
-    } else if (value == 'Del') {
+    } else if (value == DEL_SYMBOL) {
       setUserInput((prevInput) => prevInput.slice(0, -1));
     } else {
-      if (userInput === "correct" || userInput === "wrong") {
+      if (userInput === TICK_MARK || userInput === CROSS_MARK) {
         return;
       }
 
@@ -74,15 +106,14 @@ export default function Home() {
       setUserInput(newValue);
       await new Promise(f => setTimeout(f, 300));
 
-      const correctAnswer = eval(card.expression).toString();
-      if (newValue.length === correctAnswer.length) {
+      if (newValue.length === card.correctAnswer().length) {
         try {
-          if (newValue === correctAnswer) {
+          if (newValue === card.correctAnswer()) {
             card.status = "pass";
-            setUserInput("correct");
+            setUserInput(TICK_MARK);
           } else {
             card.status = "fail";
-            setUserInput("wrong");
+            setUserInput(CROSS_MARK);
           }
           await new Promise(f => setTimeout(f, 500));
 
@@ -97,11 +128,27 @@ export default function Home() {
     }
   }
 
+  const handleOperationButtonClick = (op: string) => {
+    let newOperator = op === "+" ? "*" : "+";
+    for (var c of cards) {
+      c.reinitialize(newOperator);
+    }
+    setOperation(op === "+" ? "x" : "+");
+    setUserInput('');
+    setCounter(0);
+    setCard(cards[0]);
+  }
+
+  const inputClassName = (colSpan:string ):string => {
+    let testColor = operation === "+" ? "text-yellow-500": "text-teal-500";
+    return `${testColor} text-4xl text-center ${colSpan} rounded-lg focus:outline-none`
+  }
+
   const buttons = [
     '7', '8', '9',
     '4', '5', '6',
     '1', '2', '3',
-    'AC', '0', 'Del'
+    ROTATE_SYMBOL, '0', DEL_SYMBOL
   ]
 
   return (
@@ -109,31 +156,38 @@ export default function Home() {
       <h1 className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl mb-4 sm:mb-6 md:mb-8 lg:mb-10 font-bold font-mono">DADDY GONZO MATH</h1>
       <div id="app" className="p-6 sm:p-12 md:px-24 lg:px-36 mx-2 sm:mx-20 md:mx-24 lg:mx-30 shadow-lg">
         <div className="grid grid-cols-12">
-          {cards.map((card, index) => (
+          {cards.map((c, index) => (
             <button
               key={index}
-              className={card.className()}
-            >{card.icon()}</button>
+              className={c.className()}
+            >{c.icon()}</button>
           ))}
         </div>
-        <input
-          type="text"
-          className="w-full text-sky-500 text-3xl text-center mt-2 rounded-lg focus:outline-none"
-          value={card.expression}
-          readOnly
-        />
-         <input
-          type="text"
-          className="w-full text-sky-500 text-3xl text-center my-2 rounded-lg focus:outline-none"
-          value={userInput}
-          readOnly
-        />
+        <div className="grid grid-cols-6 gap-2 my-2">
+          <input
+            type="text"
+            className={inputClassName("col-span-3")}
+            value={card.expression()}
+            readOnly
+          />
+          <input
+            type="text"
+            className={inputClassName("col-span-2")}
+            value={userInput}
+            readOnly
+          />
+          <button
+            key="opButton"
+            onClick={() => handleOperationButtonClick(operation)}
+            className="text-sky-500 bg-sky-500/25 hover:bg-sky-200/25 rounded-lg"
+          >+ \ x</button>
+        </div>
         <div className="grid grid-cols-3 gap-2">
           {buttons.map((btn) => (
             <button
               key={btn}
               onClick={() => handleButtonClick(btn)}
-              className="text-sky-600 text-3xl bg-sky-200 hover:bg-sky-300 rounded-lg"
+              className="text-3xl text-sky-600 bg-sky-200 hover:bg-sky-300 rounded-lg"
             >{btn}</button>
           ))}
         </div>
