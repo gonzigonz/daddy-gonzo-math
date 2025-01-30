6c2b8f1f4cffccee0b1e1368134ed4b58b977fa1
Gonzalo Lucero <gonzigonz@users.noreply.github.com>
2025-01-30 00:05:42 +0000

    Update Bailey's flash cards

--- app/flash_cards/bailey.tsx
+++ app/flash_cards/bailey.tsx
@@ -1,7 +1,7 @@
 import { ICard, MultiplicationCard } from "./card";
 
 export const baileysCards: ICard[] = [];
-let factors: number[] = [2, 3, 4, 5, 6, 7, 8, 9, 11, 12];
+let factors: number[] = [2, 3, 4, 8, 9];
 let secondTerms: number[] = [2, 3, 4, 5, 6, 7, 8, 9, 11, 12]
 let idx: number = 0;
 for (let f of factors) {
