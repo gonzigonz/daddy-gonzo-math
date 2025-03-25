b8f3df5fd4a4cb7a055ab69bb1f4a51b4360b2e0
Gonzalo Lucero <gonzigonz@users.noreply.github.com>
2025-03-25 16:03:15 +0000

    Shuffle Indiana's Cards

--- app/flash_cards/indiana.tsx
+++ app/flash_cards/indiana.tsx
@@ -3,11 +3,16 @@ import { ICard, MultiplicationCard } from "./card";
 export const indianasCards: ICard[] = [];
 const factors: number[] = [4];
 const secondTerms: number[] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-
 let idx: number = 0;
 for (let f of factors) {
     for (let s of secondTerms) {
         indianasCards[idx] = new MultiplicationCard(f, s);
         idx++;
     }
+}
+
+// shuffle
+for (let index = indianasCards.length - 1; index > 0; index--) {
+  const j = Math.floor(Math.random() * (index + 1));
+  [indianasCards[index], indianasCards[j]] = [indianasCards[j], indianasCards[index]]
 }
