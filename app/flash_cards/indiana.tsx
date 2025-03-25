7f8c1345ff6e25e5e2701c7bebf06ef611153a67
Gonzalo Lucero <gonzigonz@users.noreply.github.com>
2025-03-25 15:52:15 +0000

    update indiana to 4s

--- app/flash_cards/indiana.tsx
+++ app/flash_cards/indiana.tsx
@@ -1,8 +1,8 @@
 import { ICard, MultiplicationCard } from "./card";
 
 export const indianasCards: ICard[] = [];
-const factors: number[] = [1, 2, 10, 11];
-const secondTerms: number[] = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+const factors: number[] = [4];
+const secondTerms: number[] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 let idx: number = 0;
 for (let f of factors) {
